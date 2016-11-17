2e364d5e39f030f3993bbfe3e187677c7b831d41
ArmandoReimer <areimer@berkeley.edu>
2016-11-16 16:41:23 -0800

    more

--- autofocus.m
+++ autofocus.m
@@ -1,42 +1,41 @@
 close all;
+
 %here i read in the data--------------------------------------------------
 n_stacks = 200;
+connection = tcpip('128.32.173.234');
+fopen(connection);
 for j = 1:n_stacks
-    t2 = tcpip('128.32.173.234')
-    fopen(t2)
+
     command = '/cli:Leica-SP8PR-WS /app:matrix /cmd:startscan';
-    fwrite(t2, command);
+    fwrite(connection, command);
 
-    loc = 'D:\MatrixScreenerImages';
-    imdate = datestr(now, 'dd_mm_yyyy')
-    imdir = [loc, 'D:\MatrixScreenerImages\3.3.0_9757\a--', imdate,'_07_45\b--S00\c--U00--V00\d--X00--Y00\AF']
-    mdir = [imdir, 'metadata\image--L0000--S00--U00--V00--J19--E00--O00--X01--Y01--T0000.ome.xml'];
-    cin = fileread(c)
-    cdir = [imdir, 'image--L0000--S00--U00--V00--J19--E00--O00--X01--Y01--T0000--Z00--C00.ome.tif']
-    imshow(cdir);
-
-    %sample_image = 'D:\Data\Armando\livemRNA\Data\PreProcessedData\2016-10-18-1A3+5-evePr_7_Mikhail\2016-10-18-1A3+5-evePr_7_Mikhail-His_056.tif';
-    d = 'D:\Data\Armando\livemRNA\Data\PreProcessedData\2016-10-18-1A3+5-evePr_7_Mikhail\';
-    sample_stack_dir = dir(d);
-    dirlength = length(sample_stack_dir);
-
+    %imdate = datestr(now, 'dd_mm_yyyy')
+    expdir = dir('D:\MatrixScreenerImages\3.3.0_9757');
+    imdir = [expdir(end), '\b--S00\c--U00--V00\d--X00--Y00\AF'];
+    zsize = 21;
+ 
     %here i do the algorithm-------------------------------------------------
     n_spotss = [];
-    for i = 1:dirlength
-        if ~isempty(strfind(sample_stack_dir(i).name, 'His'))
-            im = imread([d, sample_stack_dir(i).name]);
-            %imshow(im, [])
+    for i = 1:zsize
+            xml = [imdir, '\metadata\image--L0000--S00--U00--V00--J19--E00--O00--X01--Y01--T0000.ome.xml'];
+            cin = fileread(c);
+            if i > 10
+                tif = [imdir, 'image--L0000--S00--U00--V00--J19--E00--O00--X01--Y01--T0000--Z',num2str(i),'--C01.ome.tif'];
+            else 
+                tif = [imdir, 'image--L0000--S00--U00--V00--J19--E00--O00--X01--Y01--T0000--Z0',num2str(i),'--C01.ome.tif'];
+            end
+            im = imread(tif);
+            imshow(im,[]);
             im2 = double(im>16);
             %imshow(im2,[]);
-            filterSize = 20 %size of square to be convolved with microscopy images
+            filterSize = 20; %size of square to be convolved with microscopy images
             dog = conv2(single(im2), single(fspecial('gaussian',filterSize, 1.5) - fspecial('gaussian',filterSize, 5)),'same');
             %dog = padarray(dog(filterSize:end-filterSize-1, filterSize:end-filterSize-1), [filterSize,filterSize]);
             dog = double(dog> .1);
             dog = imgaussfilt(dog,3);
             imshow(dog,[]);
             [im_label, n_spots] = bwlabel(im2bw(dog));
-            n_spotss(i) = n_spots
-        end
+            n_spotss(i) = n_spots;
     end
     [max, max_index] = max(n_spotss);
 
@@ -51,10 +50,9 @@ for j = 1:n_stacks
     zpos = 100; %extract this position from metadata
     %here go the commands to leica-------------------------------------------------
 
-    t2 = tcpip('128.32.173.234')
-    fopen(t2)
+
     command = ['/cli:Leica /app:matrix /sys:0 /cmd:setposition /typ:absolute',...
                 '/dev:zdrive /unit:microns /zpos:', num2str(zpos)];
-    fwrite(t2, command);
-    fclose(t2);
+    fwrite(connection, command);
 end
+fclose(connection);
