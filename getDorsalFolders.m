db6f94c59c8008680ab34434c17998361741dc97
ArmandoReimer <armandoreimer@gmail.com>
2020-03-06 07:24:55 -0800

    moved files around

--- getDorsalFolders.m
+++
@@ -1,7 +0,0 @@
-function [dataFolder, resultsFolder] = getDorsalFolders()
-
-    configValues = csv2cell('ComputerFolders.csv', 'fromfile');
-    dataFolder = getConfigValue(configValues, 'DorsalSynthetics');
-    resultsFolder = getConfigValue(configValues, 'DorsalSyntheticsDropbox');
-
-end
--- mat3DtoTif3D.m
+++
@@ -1,7 +0,0 @@
-function mat3DtoTif3D(mat3D, PreProcFolder, Prefix)
-
-for f = 1:size(mat3D, 1)
-    imwrite(uint16(squeeze(mat3D(f, :, :))), [PreProcFolder, filesep, Prefix, filesep, Prefix, '_hisMovie.tif'], 'WriteMode', 'append')
-end
-
-end
